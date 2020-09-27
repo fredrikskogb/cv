4dff484f0134f6a3fb4b53eade22b151c6ee2657
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2020-09-27 20:15:59 +0200

    added more fields of info

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("hej");
+
+    function showExperienceContainers () {
+        const experienceContainers = document.getElementsByClassName('experience-container');
+        for(let i = 0; i < experienceContainers.length; i++) {
+            experienceContainers[i].addEventListener('click', e => {
+                if(e.target.classList.contains('expand')) {
+                    show(e.target);
+                    activeToggle(e.target);
+                }
+            })
+        }
+    }
+
+    function show (el) {
+        const sibling = el.nextElementSibling;
+        if (sibling.classList.contains('show')) {
+            sibling.classList.remove('show');
+        } else {
+            sibling.classList.add('show');
+        }
+    }
+
+    function activeToggle (el) {
+        console.log(el);
+        el.classList.contains('active')
+            ? el.classList.remove('active')
+            : el.classList.add('active');
+    }
+
+    showExperienceContainers();
+
 });
